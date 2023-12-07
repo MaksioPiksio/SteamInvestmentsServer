db1f54dfb26eb58148b4fc3bfd7e56ea6bed3abf
MaksioPiksio <maksio.piksio@icloud.com>
2023-12-07 17:50:23 +0100

    Optimization | removed not used variables

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 import express from "express";
 import cors from "cors";
 import request from "request";
-import dotenv from "dotenv";
-
-dotenv.config();
 const app = express();
 
 app.use(cors());
 
-app.get("/:goods_id", (req, res) => {
-    const { goods_id } = req.params;
-    const url = `https://buff.163.com/api/market/goods/sell_order?game=csgo&goods_id=${goods_id}`;
-    req.pipe(request(url)).pipe(res);
-});
+app.get("/:goods_id", (req, res) =>
+    req
+        .pipe(
+            request(
+                `https://buff.163.com/api/market/goods/sell_order?game=csgo&goods_id=${req.params.goods_id}`
+            )
+        )
+        .pipe(res)
+);
 
 app.listen(8459);
 
